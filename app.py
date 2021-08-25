a94937c06238c10e75e2597011bd39f12c13d270
pulatov03 <pulatovubaydulla@gmail.com>
2021-08-25 18:18:52 +0500

    navbar step 1

--- app.py
+++ app.py
@@ -14,3 +14,8 @@ def main():
 def main_lt():
     lang_info = info.uz_ciril
     return render_template('index.html', information = lang_info)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- info.py
+++ info.py
@@ -1,2 +1,6 @@
 uz_lotin = dict()
+uz_lotin['header'] = ['Bosh sahifa', 'Ehrom', ['Haj amali', '1 kun', '2 kun', '3 kun', '4 kun', '5 kun'], 'Umra amali', 'Dasturni yuklab olish']
+
+
 uz_ciril = dict()
+uz_ciril['header'] = ['Бош саҳифа', 'Еҳром', ['Ҳаж амали', '1 кун', '2 кун', '3 кун', '4 кун', '5 кун'], 'Умра амали', 'Дастурни юклаб олиш']
